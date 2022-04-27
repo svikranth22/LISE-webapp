1e88a8ea040bfc0fbff70869f4cb78cd73b599b7
Vikranth Kanthamneni <vikranth@debian.attlocal.net>
2022-04-27 19:41:28 -0400

    minor updates

--- lisewebapp/app.js
+++ lisewebapp/app.js
@@ -1,18 +1,34 @@
 const express = require('express')
-const app = express()
-
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 const ip = require('ip')
 const ipAddress = ip.address()
 const port = 3000
 
+const app = express()
+
 app.set('views', './views')
 app.set('view engine', 'pug')
 
-app.get('/', (req, res) => {
+app.get('/', function (req, res) {
     res.render('lise-input')
 })
 
-app.listen(port, () => {
+app.post('/input', upload.single('file'), function (req, res) {
+    //run ./lise -f <uploaded file>
+    //respond with results/pdbid_top10.pdb
+    /* Next steps would be to show results with
+     * Jsmol
+     */
+})
+
+app.get('/input', function (req, res) {
+    /* check if results/pdbid_top10.pdb exists
+     * if it does respond with it.
+     */
+})
+
+app.listen(port, function () {
     console.log('App listening on port: ' + port)
-    console.log('Network access via: ' + ipAddress + ':' + port)
+    console.log('Network access via: http://' + ipAddress + ':' + port)
 })
